09e57f2727183adf28be1a4df25d1a52c768d96a
yooshima <o.shimayu891@gmail.com>
2025-03-02 16:51:15 +0900

    :+1: client->server msg send

--- client/main.cpp
+++ client/main.cpp
@@ -1,37 +1,34 @@
 #include "Client.hpp"
 
-int count = 0;
-
 void* SendTask(void* arg) {
   Client* sock = (Client*)arg;
   while (true) {
-    std::string test =
-        "hello, this is client. to server" + std::to_string(count);
-    count++;
-    sock->SendText(test.c_str());
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::string str;
+    std::getline(std::cin, str);
+    sock->SendText(str.c_str());
+    if (str == "exit") exit(0);
   }
 }
 
-void* RecvTask(void* arg) {
-  Client* sock = (Client*)arg;
-  while (true) {
-    char buf[1024];
-    sock->RecvText(buf);
-    std::cout << "Receive: " << buf << std::endl;
-  }
-}
+// void* RecvTask(void* arg) {
+//   Client* sock = (Client*)arg;
+//   while (true) {
+//     char buf[1024];
+//     sock->RecvText(buf);
+//     std::cout << "Receive: " << buf << std::endl;
+//   }
+// }
 
 int main(void) {
   std::cout << "Client start" << std::endl;
   pthread_t threadSendId;
-  pthread_t threadRecvId;
+  // pthread_t threadRecvId;
   int port_server = 8080;
   const char* ip_server = "127.0.0.1";
   Client client(ip_server, port_server);
   client.Connect();
   pthread_create(&threadSendId, NULL, SendTask, &client);
-  pthread_create(&threadRecvId, NULL, RecvTask, &client);
+  // pthread_create(&threadRecvId, NULL, RecvTask, &client);
   pthread_join(threadSendId, NULL);
-  pthread_join(threadRecvId, NULL);
+  // pthread_join(threadRecvId, NULL);
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,22 +1,24 @@
 #include "Server.hpp"
 
-int c = 0;
+// int c = 0;
 
-void* SendTask(void* arg) {
-  Server* sock = (Server*)arg;
-  while (true) {
-    std::string text = "server->client" + std::to_string(c);
-    c++;
-    sock->SendText(text.c_str());
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
-}
+// void* SendTask(void* arg) {
+// (void)arg;
+// Server* sock = (Server*)arg;
+// while (true) {
+// std::string text = "server->client" + std::to_string(c);
+// c++;
+// sock->SendText(text.c_str());
+// std::this_thread::sleep_for(std::chrono::seconds(1));
+// }
+// }
 
 void* RecvTask(void* arg) {
   Server* sock = (Server*)arg;
   while (true) {
     char buf[1024];
     sock->RecvText(buf);
+    if (!std::strcmp(buf, "exit")) exit(0);
     std::cout << "Receive: " << buf << std::endl;
   }
 }
@@ -26,10 +28,10 @@ int main(void) {
   server.start();
   server.connect();
 
-  pthread_t threadSendId;
+  // pthread_t threadSendId;
   pthread_t threadRecvId;
-  pthread_create(&threadSendId, NULL, SendTask, &server);
+  // pthread_create(&threadSendId, NULL, SendTask, &server);
   pthread_create(&threadRecvId, NULL, RecvTask, &server);
-  pthread_join(threadSendId, NULL);
+  // pthread_join(threadSendId, NULL);
   pthread_join(threadRecvId, NULL);
 }
